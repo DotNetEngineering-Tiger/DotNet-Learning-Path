46a6bc9e1323def9f477d794c12451af92a68151
Passang001 <116321387+Passang001@users.noreply.github.com>
2024-11-10 21:35:19 -0500

    Update TaskManagement-WebService/WebApplication1/DTO/TaskStatusDTO.cs
    Co-authored-by: praveendraa <162933532+praveendraa@users.noreply.github.com>

--- TaskManagement-WebService/WebApplication1/DTO/TaskStatusDTO.cs
+++ TaskManagement-WebService/WebApplication1/DTO/TaskStatusDTO.cs
@@ -3,7 +3,6 @@ namespace WebApplication1.DTO
 {
 public class TaskStatusDTO
     {
-		
         public int TaskId { get; set; }
         public string Title { get; set; }
         public string Status { get; set; } // Current status
