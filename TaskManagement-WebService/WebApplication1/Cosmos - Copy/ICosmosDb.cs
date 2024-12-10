9865dafa125443484f1eb4a39f808d87cbb2a58b
praveendraa <praveendra.acharya@yahoo.com>
2024-12-09 21:50:48 -0500

    Adding cosmos

---
+++ TaskManagement-WebService/WebApplication1/Cosmos - Copy/ICosmosDb.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Cosmos
+{
+    public interface ICosmosDb
+    {
+        public Task AddOrUpdate(string containerName, Object data);
+
+        public Task getItemById(string containerName, string Id);
+
+        public Task GetItems(string containerName);
+    }
+}
