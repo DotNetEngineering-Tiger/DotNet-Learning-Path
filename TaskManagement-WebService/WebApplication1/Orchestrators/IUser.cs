15ef20986bf232a5f81a46ec535f190a69d5d03c
praveendraa <praveendra.acharya@yahoo.com>
2024-12-09 22:20:01 -0500

    adding interface for User

---
+++ TaskManagement-WebService/WebApplication1/Orchestrators/IUser.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication1.DTO;
+
+namespace WebApplication1.Orchestrators
+{
+    public interface IUser
+    {
+        public Task<List<UserDTO>> GetUsers();
+
+        public Task<string> UpdateUsers(UserUpdateDTO update_users);
+
+        public Task<UserDTO> GetUserByID (string userID);
+
+       
+    }
+}
---
+++ TaskManagement-WebService/WebApplication1/Orchestrators/User.cs
@@ -0,0 +1,29 @@
+﻿using WebApplication1.Cosmos;
+using WebApplication1.DTO;
+
+namespace WebApplication1.Orchestrators
+{
+    public class User : IUser
+    {
+        private readonly ICosmosDb _cosmos;
+        private readonly string userContainer = "user";
+        public User(ICosmosDb cosmos) 
+        {
+            _cosmos = cosmos;
+        }
+        public Task<UserDTO> GetUserByID(string userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserDTO>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpdateUsers(UserUpdateDTO update_users)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
